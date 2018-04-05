283c6c96a81877f870cb8678e804f6c9f328c04b
Truong <tgv99hy@gmail.com>
2018-04-05 16:20:51 +0700

    Thêm Product Kinh thoi trang

--- js/all_var.js
+++ js/all_var.js
@@ -27,14 +27,50 @@ var $san_pham=[
 		{brand:'bentley_eyewear', name_menu: 'Bentley B-001 WG', name_display: '<div class="name_display">Bentley B-001 WG</div>', image: '<img class="product_image" src="products/bentley_eyewear/Bentley B-001 WG(321.5m).jpg"/>', price: '<div class="price">321.500.000<sup>đ</sup></div>'},
 		{brand:'bentley_eyewear', name_menu: 'Bentley B-007 RGD', name_display: '<div class="name_display">Bentley B-007 RGD</div>', image: '<img class="product_image" src="products/bentley_eyewear/Bentley B-007 RGD(300m).jpg"/>', price: '<div class="price">300.000.000<sup>đ</sup></div>'},
 
+
+
 		{brand:'estede', name_menu: 'Estede E-004 WG', name_display: '<div class="name_display">Estede E-004 WG(177m)</div>', image:'<img class="product_image" src="products/estede/E-004 WG(177m).jpg"/>', price: '<div class="price">100.000.000<sup>đ</sup></div>'},
 		{brand:'estede', name_menu: 'Estede E-007 RG', name_display: '<div class="name_display">Estede E-007 RG(153m)</div>', image:'<img class="product_image" src="products/estede/E-007 RG(153m).jpg"/>', price: '<div class="price">200.000.000<sup>đ</sup></div>'},
 
-		{brand:'linda_farrow', name_menu: '', name_display: '', image: '', price: ''},
 
-		{brand:'baton_perreira', name_menu: '', name_display: '', image: '', price: ''},
 
-		{brand:'ralph_vaessen', name_menu: '', name_display: '', image: '', price: ''},
+		{brand:'linda_farrow', name_menu: 'LINDA FARROW LUXE 166C26', name_display: '<div class="name_display">LINDA FARROW LUXE 166C26</div>', image: '<img class="product_image" src="products/linda_farrow/LINDA FARROW LUXE 166C26(16M).jpg "/>', price: '<div class="price">16.000.000<sup>đ</sup></div>'},
+		{brand:'linda_farrow', name_menu: 'LINDA FARROW LUXE 166C26', name_display: '<div class="name_display">LINDA FARROW LUXE 166C26</div>', image: '<img class="product_image" src="products/linda_farrow/LINDA FARROW LUXE 166C26(22M).jpg "/>', price: '<div class="price">22.000.000<sup>đ</sup></div>'},
+		{brand:'linda_farrow', name_menu: 'LINDA FARROW LUXE 251C60', name_display: '<div class="name_display">LINDA FARROW LUXE 251C60</div>', image: '<img class="product_image" src="products/linda_farrow/LINDA FARROW LUXE 251C60(16M).jpg "/>', price: '<div class="price">16.000.000<sup>đ</sup></div>'},
+		{brand:'linda_farrow', name_menu: 'LINDA FARROW LUXE 307C3', name_display: '<div class="name_display">LLINDA FARROW LUXE 307C3</div>', image: '<img class="product_image" src="products/linda_farrow/LINDA FARROW LUXE 307C3(32M).jpg "/>', price: '<div class="price">32.000.000<sup>đ</sup></div>'},
+		{brand:'linda_farrow', name_menu: 'LINDA FARROW LUXE 538C3', name_display: '<div class="name_display">LINDA FARROW LUXE 538C3</div>', image: '<img class="product_image" src="products/linda_farrow/LINDA FARROW LUXE 538C3(23M).jpg "/>', price: '<div class="price"23.000.000<sup>đ</sup></div>'},
+		{brand:'linda_farrow', name_menu: 'LINDA FARROW LUXE S2C2', name_display: '<div class="name_display">LINDA FARROW LUXE S2C2</div>', image: '<img class="product_image" src="products/linda_farrow/LINDA FARROW LUXE S2C2(23M).jpg "/>', price: '<div class="price">23.000.000<sup>đ</sup></div>'},
 
-		{brand:'mykita', name_menu: '', name_display: '', image: '', price: ''}
+
+
+		{brand:'lunor', name_menu: 'LUNOR - A6 250 - 18', name_display: '<div class="name_display">LUNOR - A6 250 - 18</div>', image: '<img class="product_image" src="products/lunor/LUNOR - A6 250 - 18(12M).jpg "/>', price: '<div class="price">12.000.000<sup>đ</sup></div>'},
+		{brand:'lunor', name_menu: 'LUNOR - S2 1202 03', name_display: '<div class="name_display">LUNOR - S2 1202 03</div>', image: '<img class="product_image" src="products/lunor/LUNOR - S2 1202 03(12M).jpg "/>', price: '<div class="price">12.000.000<sup>đ</sup></div>'},
+		{brand:'lunor', name_menu: 'LUNOR - V109 PP', name_display: '<div class="name_display">LUNOR - V109 PP</div>', image: '<img class="product_image" src="products/lunor/LUNOR - V109 PP(14M).jpg "/>', price: '<div class="price">14.000.000<sup>đ</sup></div>'},
+		{brand:'lunor', name_menu: 'LUNOR - VA 108 AS', name_display: '<div class="name_display">LUNOR - VA 108 AS</div>', image: '<img class="product_image" src="products/lunor/LUNOR - VA 108 AS(15M).jpg "/>', price: '<div class="price">15.000.000<sup>đ</sup></div>'},
+		{brand:'lunor', name_menu: 'LUNOR - XV1503 - RG', name_display: '<div class="name_display">LUNOR - XV1503 - RG</div>', image: '<img class="product_image" src="products/lunor/LUNOR - XV1503 - RG(12M).jpg "/>', price: '<div class="price">12.000.000<sup>đ</sup></div>'},
+
+
+
+		{brand:'barton_perreira', name_menu: 'GRIFFIN - TOKYO TORTOISEANTIQUE GOLD', name_display: '<div class="name_display">GRIFFIN - TOKYO TORTOISEANTIQUE GOLD</div>', image: '<img class="product_image" src="products/barton_perreira/GRIFFIN - TOKYO TORTOISEANTIQUE GOLD(13M).jpg "/>', price: '<div class="price">13.000.000<sup>đ</sup></div>'},
+		{brand:'barton_perreira', name_menu: 'NEAL - MATTE CHESTNUT', name_display: '<div class="name_display">NEAL - MATTE CHESTNUT</div>', image: '<img class="product_image" src="products/barton_perreira/NEAL - MATTE CHESTNUT(10M).jpg "/>', price: '<div class="price">10.000.000<sup>đ</sup></div>'},
+		{brand:'barton_perreira', name_menu: 'RHIANNON - DESERT SKYPEWTER', name_display: '<div class="name_display">RHIANNON - DESERT SKYPEWTER</div>', image: '<img class="product_image" src="products/barton_perreira/RHIANNON - DESERT SKYPEWTER(12M).jpg "/>', price: '<div class="price">12.000.000<sup>đ</sup></div>'},
+		{brand:'barton_perreira', name_menu: 'RONSON - BLACKMATTE BLACK', name_display: '<div class="name_display">RONSON - BLACKMATTE BLACK</div>', image: '<img class="product_image" src="products/barton_perreira/RONSON - BLACKMATTE BLACK(13M).jpg "/>', price: '<div class="price">13.000.000<sup>đ</sup></div>'},
+
+
+
+		{brand:'ralph_vaessen', name_menu: 'PAUL 910 - RALPH VAESSEN', name_display: '<div class="name_display">PAUL 910 - RALPH VAESSEN</div>', image: '<img class="product_image" src="products/ralph_vensen/PAUL 910 - RALPH VAESSEN(30M).jpg "/>', price: '<div class="price">30.000.000<sup>đ</sup></div>'},
+		{brand:'ralph_vaessen', name_menu: 'RALPH VAESSEN - CAREL', name_display: '<div class="name_display">RALPH VAESSEN - CAREL</div>', image: '<img class="product_image" src="products/ralph_vensen/RALPH VAESSEN - CAREL(32M).jpg "/>', price: '<div class="price">32.000.000<sup>đ</sup></div>'},
+		{brand:'ralph_vaessen', name_menu: 'RALPH VAESSEN - ISOLDE', name_display: '<div class="name_display">RALPH VAESSEN - ISOLDE</div>', image: '<img class="product_image" src="products/ralph_vensen/RALPH VAESSEN - ISOLDE(30M).jpg "/>', price: '<div class="price">30.000.000<sup>đ</sup></div>'},
+		{brand:'ralph_vaessen', name_menu: 'RALPH VAESSEN - SAAR', name_display: '<div class="name_display">RALPH VAESSEN - SAAR</div>', image: '<img class="product_image" src="products/ralph_vensen/RALPH VAESSEN - SAAR(28M).jpg "/>', price: '<div class="price">28.000.000<sup>đ</sup></div>'},
+
+
+		{brand:'von_akel', name_menu: 'Von Arkel 0101-006', name_display: '<div class="name_display">Von Arkel 0101-006</div>', image: '<img class="product_image" src="products/von_arkel/Von Arkel 0101-006(17M).jpg "/>', price: '<div class="price">17.000.000<sup>đ</sup></div>'},
+		{brand:'von_akel', name_menu: 'Von Arkel 0305-002', name_display: '<div class="name_display">Von Arkel 0305-002</div>', image: '<img class="product_image" src="products/von_arkel/Von Arkel 0305-002(12M).jpg "/>', price: '<div class="price">12.000.000<sup>đ</sup></div>'},
+		{brand:'von_akel', name_menu: 'Von Arkel 0416-029', name_display: '<div class="name_display">Von Arkel 0416-029</div>', image: '<img class="product_image" src="products/von_arkel/Von Arkel 0416-029(14.2M).jpg "/>', price: '<div class="price">14.200.000<sup>đ</sup></div>'},
+		{brand:'von_akel', name_menu: 'Von Arkel 1701-002', name_display: '<div class="name_display">Von Arkel 1701-002</div>', image: '<img class="product_image" src="products/von_arkel/Von Arkel 1701-002(10M).jpg "/>', price: '<div class="price">10.000.000<sup>đ</sup></div>'},
+		{brand:'von_akel', name_menu: 'Von Arkel 1702-001', name_display: '<div class="name_display">Von Arkel 1702-001</div>', image: '<img class="product_image" src="products/von_arkel/Von Arkel 1702-001(10M).jpg "/>', price: '<div class="price">10.000.000<sup>đ</sup></div>'},
+
+
+		{brand:'mikita', name_menu: 'MYKITA+MAISON MARGIELA - MMTRANS001 - SILVERORBLACK PYTHON', name_display: '<div class="name_display">MYKITA+MAISON MARGIELA - MMTRANS001 - SILVERORBLACK PYTHON</div>', image: '<img class="product_image" src="products/von_arkel/MYKITA+MAISON MARGIELA - MMTRANS001 - SILVERORBLACK PYTHON.jpg "/>', price: '<div class="price">15.000.000<sup>đ</sup></div>'},
+
 		 ]; 
